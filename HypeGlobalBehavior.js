f3193153b17e8bef82f9572bfa2bfa06c1a963ac
Max Ziebell <mail@maxziebell.de>
2020-06-21 01:17:54 +0200

    Update HypeGlobalBehavior.js

--- HypeGlobalBehavior.js
+++ HypeGlobalBehavior.js
@@ -29,8 +29,7 @@ copyright (c) 2018 Max Ziebell, (https://maxziebell.de). MIT-license
  * @property {function} stopAllCustomBehaviorTicker This function allows to stop all time based global behavior ticker from the window scope. The signatur is equal to hypeDocument.stopAllCustomBehaviorTicker
  */
 
-if("HypeGlobalBehavior" in window === false) 
-	window['HypeGlobalBehavior'] = (function () {
+if("HypeGlobalBehavior" in window === false) window['HypeGlobalBehavior'] = (function () {
 	
 	/* Lookup for running intervals by behavior name */
 	var _ticker = {};
